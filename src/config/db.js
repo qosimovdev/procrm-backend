c3e19795d6d977e4db7ff901d64cbf7cab898839
ibrohim3 <ibrohim.2128382@gmail.com>
2026-06-22 15:04:09 +0500

    update

--- src/config/db.js
+++ src/config/db.js
@@ -1,32 +1,32 @@
-const { Sequelize } = require("sequelize")
-require("dotenv").config()
-
-const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PASSWORD,
-    {
-        host: process.env.DB_HOST,
-        dialect: "postgres"
-    }
-)
-
-module.exports = sequelize
-
-// const { Sequelize } = require("sequelize");
+// const { Sequelize } = require("sequelize")
 // require("dotenv").config()
 
 // const sequelize = new Sequelize(
-//     process.env.DATABASE_URL,
+//     process.env.DB_NAME,
+//     process.env.DB_USER,
+//     process.env.DB_PASSWORD,
 //     {
-//         dialect: "postgres",
-//         dialectOptions: {
-//             ssl: {
-//                 require: true,
-//                 rejectUnauthorized: false,
-//             },
-//         },
+//         host: process.env.DB_HOST,
+//         dialect: "postgres"
 //     }
-// );
+// )
 
-// module.exports = sequelize;
+// module.exports = sequelize
+
+const { Sequelize } = require("sequelize");
+require("dotenv").config()
+
+const sequelize = new Sequelize(
+    process.env.DATABASE_URL,
+    {
+        dialect: "postgres",
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false,
+            },
+        },
+    }
+);
+
+module.exports = sequelize;
